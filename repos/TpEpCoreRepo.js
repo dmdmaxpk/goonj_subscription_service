c2ab23fb900a1e1107c02db4ebf27ced02abe5c3
Taha <taha@dmdmax.com.pk>
2021-08-05 15:11:39 +0500

    DC request body updated

--- repos/TpEpCoreRepo.js
+++ repos/TpEpCoreRepo.js
@@ -4,8 +4,8 @@ const config = require('../config');
 class TpEpCoreRepository{
     
     async processDirectBilling(otp, user, subscriptionObj, packageObj, bool){
-        // console.log("direct billing api call", otp, user, subscriptionObj, packageObj, bool);
-        return await Axios.post(`${config.servicesUrls.tp_ep_core_service}/core/charge`, {otp, msisdn: user.msisdn, payment_source: user.operator, amount: packageObj.price_point_pkr, bool})
+        console.log("direct billing api call", otp, user, subscriptionObj, packageObj, bool);
+        return await Axios.post(`${config.servicesUrls.tp_ep_core_service}/core/charge`, {otp, msisdn: user.msisdn, payment_source: user.operator, amount: packageObj.price_point_pkr, transaction_id: bool, partner_id: packageObj.partner_id})
         .then(res =>{ 
             let result = res.data;
             return result
