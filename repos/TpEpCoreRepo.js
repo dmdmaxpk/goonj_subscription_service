02ef4be55725336b0aa457568472ba4d4a9b09fd
Taha <taha@dmdmax.com.pk>
2021-08-05 16:28:26 +0500

    nanoid to shortid

--- repos/TpEpCoreRepo.js
+++ repos/TpEpCoreRepo.js
@@ -1,12 +1,12 @@
 const Axios = require('axios');
-const nanoid = require('nanoid');
+const shortid = require('shortid');
 const config = require('../config');
 
 class TpEpCoreRepository{
     
     async processDirectBilling(otp, user, subscriptionObj, packageObj, bool){
         // console.log("direct billing api call", otp, user, subscriptionObj, packageObj, bool);
-        let transaction_id = user.msisdn + '_' + user._id + nanoid(10);
+        let transaction_id = user.msisdn + '_' + user._id + shortid.generate();
         return await Axios.post(`${config.servicesUrls.tp_ep_core_service}/core/charge`, {otp, msisdn: user.msisdn, payment_source: user.operator, amount: packageObj.price_point_pkr, transaction_id, partner_id: packageObj.partner_id})
         .then(res =>{ 
             let result = res.data;
