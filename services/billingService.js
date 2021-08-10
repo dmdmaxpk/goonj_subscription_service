06351de2f03f191bb321ffd866f1f90561204af1
Taha <taha@dmdmax.com.pk>
2021-08-10 12:57:23 +0500

    logs added

--- services/billingService.js
+++ services/billingService.js
@@ -101,6 +101,7 @@ async billingFailed(user, subscription, response, packageObj, transaction_id, fi
     console.log("billing failed: subscription obj", subscription._id, 'first_time_billing', first_time_billing);
 
     let checkSubscription = await this.subscriptionRepository.getSubscriptionByPackageId(user._id, packageObj._id);
+    console.log("warning", "checkSubscription: ", checkSubscription, "type ", typeof checkSubscription);
     if(checkSubscription.length < 1){
         subscription.subscription_status = 'none';
         subscription.is_allowed_to_stream = false;
