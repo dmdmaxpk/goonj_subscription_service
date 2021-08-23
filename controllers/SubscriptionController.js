b7f0f5409ed1e2ece4b95dc92dfc6ee412b1a458
taha <taha@dmdmax.com>
2021-08-23 04:44:46 -0700

    addition

--- controllers/SubscriptionController.js
+++ controllers/SubscriptionController.js
@@ -549,7 +549,7 @@ doSubscribeUsingSubscribingRuleAlongWithMicroCharging = async(otp, source, user,
 			}
 			subscriptionObj.subscribed_package_id = packageObj._id;
 			console.log("otp", otp)
-			let result = await tpEpCoreRepo.processDirectBilling(subscriptionObj.ep_token ? undefined : otp, user, subscriptionObj, packageObj, true);
+			let result = await tpEpCoreRepo.processDirectBilling(subscriptionObj.ep_token ? undefined : otp, user, subscriptionObj, packageObj, true, true);
 			if(result.message === "success"){
 				dataToReturn.status = "charged";
 				dataToReturn.subscriptionObj = subscriptionObj;
