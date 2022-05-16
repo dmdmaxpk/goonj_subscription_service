1f36c48136bc43a9deb3343e964611b0aa08a328
Farhan Ali <farhan.ali@dmdmax.com>
2022-05-16 16:20:48 +0500

    updated tid

--- services/billingService.js
+++ services/billingService.js
@@ -164,16 +164,22 @@ class BillingService{
             url = config.ideation_Affpro_callback + `${tid}`;
         } else if (mid === "1" || mid === "gdn" ){
             return new Promise((resolve,reject) => { reject(null)})
+        }else if(mid === 'affpro') {
+            url = "http://ad.propellerads.com/conversion.php?aid=3541543&pid=&tid=108058&visitor_id="+tid
+
         }
-        console.log("warning", "affiliate url",url)
+
+        console.log("warning", "affiliate url", url)
         return new Promise(function(resolve, reject) {
             axios({
                 method: 'post',
                 url: url,
                 headers: {'Content-Type': 'application/x-www-form-urlencoded' }
             }).then(function(response){
+                console.log("affpro", response.data);
                 resolve(response.data);
             }).catch(function(err){
+                console.log("affpro - err", err.message);
                 reject(err);
             });
         });
