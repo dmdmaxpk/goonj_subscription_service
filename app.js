6c49237559362db81a2a8d727253198506ccf5c2
taha <taha@dmdmax.com>
2022-07-06 01:26:47 +0500

    update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Import database models
 require('./models/Subscription');
-require('./models/Waleelogs');
+require('./models/WaleeLogs');
 
 // Connection to Database
 mongoose.connect(config.mongo_connection_url, {useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false});
